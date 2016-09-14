1b316e37555c9461a03c304df87651e5f159627d
anzizhao <anzizhao@126.com>
2016-09-14 14:34:49 +0800

    功能  店员订单详情完成

--- test/data/mockdata.js
+++ test/data/mockdata.js
@@ -205,25 +205,27 @@ module.exports = function() {
             }
         },
         detailList: {
-            status: false,
+            status: true,
             error: 0,
             msg: '',
-            data: [
-                {
-                    date: '2016年03月12日',
-                    totalPaidIn: '232', //金额总数
-                    details: [
-                        {
-                            orderAmount: '25', 
-                            coupon: '0.4',
-                            paidIn: '123',
-                            time: '14:42:23'
-                        },
-                        {
-                            orderAmount: '125', 
-                            coupon: '10.4',
-                            paidIn: '223',
-                            time: '12:42:23'
+            data:  {
+                status: true, 
+                list:  [
+                    {
+                        date: '2016年03月12日',
+                        totalPaidIn: '232', //金额总数
+                        details: [
+                            {
+                                orderAmount: '25', 
+                                coupon: '0.4',
+                                paidIn: '123',
+                                time: '14:42:23'
+                            },
+                            {
+                                orderAmount: '125', 
+                                coupon: '10.4',
+                                paidIn: '223',
+                                time: '12:42:23'
                         },
 						]
 					},
@@ -258,6 +260,123 @@ module.exports = function() {
 						]
 					}
                 ]
-        }
+            }
+        },
+        detailList_123: {
+            status: true,
+            error: 0,
+            msg: '',
+            data:  {
+                status: false, 
+                list: [
+                    {
+                        date: '2016年03月10日',
+                        totalPaidIn: '132', //金额总数
+                        details: [
+                            {
+                                orderAmount: '205', 
+                                coupon: '20.4',
+                                paidIn: '223',
+                                time: '10:42:23'
+                            },
+                            {
+                                orderAmount: '25', 
+                                coupon: '0.4',
+                                paidIn: '123',
+                                time: '14:42:23'
+                            },
+                            {
+                                orderAmount: '125', 
+                                coupon: '10.4',
+                                paidIn: '223',
+                                time: '12:42:23'
+                            },
+                        ]
+                    },
+                    {
+                        date: '2016年03月09日',
+                        totalPaidIn: '132', //金额总数
+                        details: [
+                            {
+                                orderAmount: '205', 
+                                coupon: '20.4',
+                                paidIn: '223',
+                                time: '10:42:23'
+                            },
+                            {
+                                orderAmount: '25', 
+                                coupon: '0.4',
+                                paidIn: '123',
+                                time: '14:42:23'
+                            },
+                            {
+                                orderAmount: '125', 
+                                coupon: '10.4',
+                                paidIn: '223',
+                                time: '12:42:23'
+                            },
+                        ]
+                    },
+                ]
+            }
+        },
+        detailList_124: {
+            status: true,
+            error: 0,
+            msg: '',
+            data:  {
+                status: false, 
+                list: [
+                    {
+                        date: '2016年03月10日',
+                        totalPaidIn: '132', //金额总数
+                        details: [
+                            {
+                                orderAmount: '205', 
+                                coupon: '20.4',
+                                paidIn: '223',
+                                time: '10:42:23'
+                            },
+                            {
+                                orderAmount: '25', 
+                                coupon: '0.4',
+                                paidIn: '123',
+                                time: '14:42:23'
+                            },
+                            {
+                                orderAmount: '125', 
+                                coupon: '10.4',
+                                paidIn: '223',
+                                time: '12:42:23'
+                            },
+                        ]
+                    },
+                    {
+                        date: '2016年03月09日',
+                        totalPaidIn: '132', //金额总数
+                        details: [
+                            {
+                                orderAmount: '205', 
+                                coupon: '20.4',
+                                paidIn: '223',
+                                time: '10:42:23'
+                            },
+                            {
+                                orderAmount: '25', 
+                                coupon: '0.4',
+                                paidIn: '123',
+                                time: '14:42:23'
+                            },
+                            {
+                                orderAmount: '125', 
+                                coupon: '10.4',
+                                paidIn: '223',
+                                time: '12:42:23'
+                            },
+                        ]
+                    },
+                ]
+            }
+        },
     }
 }
