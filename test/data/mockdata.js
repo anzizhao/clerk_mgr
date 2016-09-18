428be4ee98047c973f1037b627ab4e4cd03c798a
anzizhao <anzizhao@126.com>
2016-09-18 16:34:48 +0800

    模拟数据使用json-server的middleware  可以很好的模拟操作的一些请求

--- test/data/mockdata.js
+++ test/data/mockdata.js
@@ -61,129 +61,6 @@ module.exports = function() {
                 ]
             }
         },
-        list_date1: {
-            error: 0,
-            msg: '',
-            data: {
-                section: [
-                    {
-                        title: '店员',
-                        eleId: true,
-                        list: [
-                            {
-                                clerkId: '123', //店员ID
-                                name: '王二', //店员姓名
-                                order_num: 23, //订单数
-                                money_num: 232.22, 	//实收金额
-                                isBound: true //是否绑定桌牌
-                            },
-                            {
-                                clerkId: '124', //店员Id
-                                name: '风清扬',
-                                order_num: 713,
-                                money_num: 712.02,
-                                isBound: false
-                            } 
-                        ]
-                    },
-                    {
-                        title: '其他',
-                        eleId: false,
-                        list: [
-                            {
-                                clerkId: '456', //店员Id
-                                name: '店长订单',
-                                order_num: 20,
-                                money_num: 152.22,
-                                isBound: false //是否绑定桌牌
-                            }
-                        ]
-                    }		    	
-                ]
-            }
-        },
-        list_date7: {
-            error: 0,
-            msg: '',
-            data: {
-                section: [
-                    {
-                        title: '店员',
-                        eleId: true,
-                        list: [
-                            {
-                                clerkId: '123', //店员ID
-                                name: '王二', //店员姓名
-                                order_num: 723, //订单数
-                                money_num: 7232.22, 	//实收金额
-                                isBound: true //是否绑定桌牌
-                            },
-                            {
-                                clerkId: '124', //店员Id
-                                name: '风清扬',
-                                order_num: 713,
-                                money_num: 712.02,
-                                isBound: false
-                            } 
-                        ]
-                    },
-                    {
-                        title: '其他',
-                        eleId: false,
-                        list: [
-                            {
-                                clerkId: '456', //店员Id
-                                name: '店长订单',
-                                order_num: 20,
-                                money_num: 152.22,
-                                isBound: false //是否绑定桌牌
-                            }
-                        ]
-                            }		    	
-                    ]
-                }
-        },
-        list_date30: {
-            error: 0,
-            msg: '',
-            data: {
-                section: [
-                    {
-                        title: '店员',
-                        eleId: true,
-                        list: [
-                            {
-                                clerkId: '123', //店员ID
-                                name: '王二', //店员姓名
-                                order_num: 3023, //订单数
-                                money_num: 30232.22, 	//实收金额
-                                isBound: true //是否绑定桌牌
-                            },
-                            {
-                                clerkId: '124', //店员Id
-                                name: '风清扬',
-                                order_num: 713,
-                                money_num: 712.02,
-                                isBound: false
-                            } 
-                        ]
-                    },
-                    {
-                        title: '其他',
-                        eleId: false,
-                                list: [
-                                    {
-                                        clerkId: '456', //店员Id
-                                        name: '店长订单',
-                                        order_num: 20,
-                                        money_num: 152.22,
-                                        isBound: false //是否绑定桌牌
-                                    }
-                                ]
-                            }		    	
-                    ]
-                    }
-        },
         detail_123: {
             error: 0,
             msg: '',
@@ -207,7 +84,6 @@ module.exports = function() {
             }
         },
         detailList: {
-            status: true,
             error: 0,
             msg: '',
             data:  {
@@ -270,8 +146,7 @@ module.exports = function() {
             }
         },
 
-        detailList_123: {
-            status: true,
+        detailList_more: {
             error: 0,
             msg: '',
             data:  {
@@ -335,70 +210,7 @@ module.exports = function() {
             }
 
         },
-        detailList_124: {
-            status: true,
-            error: 0,
-            msg: '',
-            data:  {
-                status: false, 
-                list: [
-                    {
-                        date: '2016年03月10日',
-                        totalPaidIn: '132', //金额总数
-                        details: [
-                            {
-                                orderAmount: '205', 
-                                coupon: '20.4',
-                                paidIn: '223',
-                                payType: '当面付',
-                                time: '10:42:23'
-                            },
-                            {
-                                orderAmount: '25', 
-                                coupon: '0.4',
-                                paidIn: '123',
-                                payType: '当面付',
-                                time: '14:42:23'
-                            },
-                            {
-                                orderAmount: '125', 
-                                coupon: '10.4',
-                                paidIn: '223',
-                                payType: '当面付',
-                                time: '12:42:23'
-                            },
-                        ]
-                    },
-                    {
-                        date: '2016年03月09日',
-                        totalPaidIn: '132', //金额总数
-                        details: [
-                            {
-                                orderAmount: '205', 
-                                coupon: '20.4',
-                                paidIn: '223',
-                                payType: '当面付',
-                                time: '10:42:23'
-                            },
-                            {
-                                orderAmount: '25', 
-                                coupon: '0.4',
-                                paidIn: '123',
-                                payType: '当面付',
-                                time: '14:42:23'
-                            },
-                            {
-                                orderAmount: '125', 
-                                coupon: '10.4',
-                                paidIn: '223',
-                                payType: '支付宝支付',
-                                time: '12:42:23'
-                            },
-                        ]
-                    },
-                ]
-            }
-        },
+
         bindCard: {
             error: 0,
             msg: '',
